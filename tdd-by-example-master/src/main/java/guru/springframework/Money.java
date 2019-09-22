068572f18e8732e72232837a10465c311ff20081
Aky <Aky@Akys-Laptop>
2019-09-22 23:15:59 +0100

    Added bank

--- tdd-by-example-master/src/main/java/guru/springframework/Money.java
+++ tdd-by-example-master/src/main/java/guru/springframework/Money.java
@@ -1,6 +1,6 @@
 package guru.springframework;
 
-public class Money {
+public class Money implements Expression {
 	protected int amount;
 	protected String currency;
 	
@@ -29,6 +29,15 @@ public class Money {
 	
 	public static Money franc(int amount) {
 		return new Money(amount,"CHF");
+	}
+	
+	public Expression plus(Money addend) {
+		return new Sum(this, addend);
+		
+	}
+	
+	public Money reduce(String to) {
+		return this;
 	}
 
 	@Override
